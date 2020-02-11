e8d7de0265a9a77c007cb0f0b73991f67f9c2365
izumin5210 <m@izum.in>
2020-02-11 19:03:10 +0900

    Remove unused spec

--- spec/scopedog_spec.rb
+++ spec/scopedog_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Scopedog do
   it "has a version number" do
     expect(Scopedog::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
